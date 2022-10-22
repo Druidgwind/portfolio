e2a450e5b80a0e0d02e637c1cb27a31b51439386
Druidgwind <lukaboy.foz@gmail.com>
2022-10-22 15:42:08 -0300

    alterações

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+
+
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -16,3 +18,21 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+
+// const body = document.querySelector("body");
+// var typed = new Typed(".type", {
+//     strings: ["Estudante", "Servidor Público", "Designer"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
+// })
+
+if (this.scrollY > 500) {
+    $('.scroll-up-btn').addClass("show");
+} else {
+    $('.scroll-up-btn').removeClass("show");
+}
+$('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
+});
